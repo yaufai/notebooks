677229e67c9d75b5517cf9a628244fbe190675a2
yaufai <youhui.dev@gmail.com>
2022-01-14 18:14:59 +0800

    save

--- isabelle-tutorial/FirstOrderLogic.thy
+++ isabelle-tutorial/FirstOrderLogic.thy
@@ -7,5 +7,24 @@ lemma "\<forall>x. P x \<longrightarrow> P x"
   apply (rule allI)
   by (rule impI)
 
+print_statement impI
+(* [ P \<Longrightarrow> Q ] \<Longrightarrow>  (P \<longrightarrow> Q) *)
+
+print_statement allI
+(* [ \<And>x. P x ] \<Longrightarrow> \<forall>x. P x *)
+
+print_statement spec
+(* [ \<forall>x. P x ] \<Longrightarrow> P x *)
+
+
+lemma "(\<forall>x. P \<longrightarrow> Q x) \<Longrightarrow> P \<longrightarrow> (\<forall>x. Q x)"
+  (* [ \<forall>x. P \<longrightarrow> Q x ] \<Longrightarrow> P \<longrightarrow> \<forall>x. Q x *)
+  apply (rule impI)
+  (* [ \<forall>x. P \<longrightarrow> Q x; P ] \<Longrightarrow> \<forall>x. Q x *)
+  apply (rule allI)
+  (*  \<And>x. [ \<forall>x. P \<longrightarrow> Q x; P ] \<Longrightarrow> Q x *)
+  (*
+  
+  *)
 
 end
